43999127c4e17d9bd4ce768890519d8bd05a7668
ryan <zltsang@gmail.com>
2018-10-17 01:07:02 +0800

    vpress source

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  title: 'toso',  // 设置网站标题
-  dest: '.vuepress/dist',    // 设置输出目录
+  title: '海水打湿了鞋',  // 设置网站标题
+  dest: '.dist',    // 设置输出目录
   base: '/', // 设置站点根路径
   repo: 'https://github.com/hitme/hitme.github.io' // 添加 github 链接
 }
